011c02807b40a3eddbe5ffb927a2d1498d827489
SrtaKennedy <ruthellen1800@gmail.com>
2023-10-13 18:30:41 -0300

    Projeto finalizado

--- script/main.js
+++ script/main.js
@@ -1,1 +1,11 @@
-let elementos = document.querySelectorAll('.duvida');
+let elementosDuvida = document.querySelectorAll('.duvida'); // pegando todos os elementos
+
+elementosDuvida.forEach(function (duvida) { // executar uma função para cada elemento
+    duvida.addEventListener('click', function() { // para cada elemento um evento de click
+        duvida.classList.toggle('ativa') // para cada evento de click, vai executar esse linha
+    })
+})
+
+/* function nome (argumento) {
+    conteudo da função
+} */
